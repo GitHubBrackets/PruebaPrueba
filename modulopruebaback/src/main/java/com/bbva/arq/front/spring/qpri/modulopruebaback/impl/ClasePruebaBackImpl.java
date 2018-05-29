acc6a39e7b60c5d6980c8556c90cad391410591f
GitHubBrackets <diego.bolanios@gmail.com>
2018-05-29 18:22:10 -0500

    update class error
    error

--- modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
+++ modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
@@ -3,5 +3,9 @@ package com.bbva.arq.front.spring.qpri.modulopruebaback.impl;
 import com.bbva.arq.front.spring.qpri.modulopruebaback.ClasePruebaBack;
 
 public ClasePruebaBackImpl implements ClasePruebaBack {
+  
+  private String valor(){
+  
+  }
 
 }
